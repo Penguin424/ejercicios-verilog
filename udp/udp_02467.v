d46f048d39efe481a2b8934db8949c56fb5fd2a9
Pablo Rizo <cosbiomedev@gmail.com>
2020-07-27 16:29:27 -0500

    Primitivos

---
+++ udp/udp_02467.v
@@ -0,0 +1,18 @@
+primitive UPD_02467(D, A, B, C);
+
+    output D;
+    input A, B, C;
+
+    table
+        //  A B C : D
+        0 0 0 : 1;
+        0 0 1 : 0;
+        0 1 0 : 1;
+        0 1 1 : 0;
+        1 0 0 : 1;
+        1 0 1 : 0;
+        1 1 0 : 1;
+        1 1 1 : 1;
+    endtable
+
+endprimitive
